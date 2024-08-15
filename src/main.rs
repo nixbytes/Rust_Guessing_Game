82ecc9b5dbf19c24512ab466b26189d5293238e2
nixbytes <real8686@gmail.com>
2024-08-14 22:22:53 -0400

    add match and learn about shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -9,9 +10,20 @@ fn main() {
     println!("The secret number is :{secret_number}");
 
     println!("Please input a number:");
+    
     let mut guess = String::new();
+    
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read input");
-    println!("You guess correctly");
+    
+    let guess: u32 = guess.trim().parse().expect("Please input a number value");
+
+    println!("You guess: {guess}");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You Win!!! :)"),
+    }
 }
